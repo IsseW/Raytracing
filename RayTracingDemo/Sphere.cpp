ef34c1e6637904c9dfb20af3822b79d595214708
IsseW <isidornie@gmail.com>
2022-02-12 16:33:20 +0100

    stuff lol

--- RayTracingDemo/Sphere.cpp
+++ RayTracingDemo/Sphere.cpp
@@ -7,13 +7,21 @@ Sphere::Sphere(const Vector3D & colour, const Vector3D c, double r) : Shape(colo
 bool Sphere::Intersection(const Ray & ray, double & t)
 {
 	Vector3D localPoint = this->center - ray.origin;
+
 	double relative_distance = localPoint * ray.direction;
+
 	double distanceToCenterSqr = localPoint * localPoint;
+
 	if (relative_distance < 0 && distanceToCenterSqr > radius * radius) return false;
+
 	double msqr = distanceToCenterSqr - relative_distance * relative_distance;
+
 	if (msqr > radius * radius) return false;
+
 	double q = std::sqrt(radius * radius - msqr);
+
 	if (distanceToCenterSqr > radius * radius) t = relative_distance - q;
 	else t = relative_distance + q;
+
 	return true;
 }
